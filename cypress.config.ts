77f515f2916f41de198d0f3628ca08c5d2e0606a
Matteo LLC <45246151+MatteoLLC@users.noreply.github.com>
2025-05-05 22:03:23 -0400

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
 	e2e: {
-		baseUrl: 'http://open-webui-three-xi.vercel.app'
+		baseUrl: 'http://localhost:8080'
 	},
 	video: true
 });
